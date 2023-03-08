2af39b47eb60d7a26ce6414b1466e6208aa29c99
yxj0312 <yanxiajun@hotmail.com>
2023-03-08 22:31:41 +0100

    add feature flag

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace App\Providers;
 
+use App\Models\User;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Pennant\Feature;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +21,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Feature::define('new-design', function(User $user) {
+            return $user->id === 1;
+        });
     }
 }
